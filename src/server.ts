8b6bd20818f4f7210c2ef1cabf310eb7e2358f4f
foultrip <davidvasquezmahecha@gmail.com>
2024-05-30 17:19:27 -0500

    cors update

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,14 @@ import app from "./app";
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000/",
+      "http://localhost:3001/",
+      "https://creditoya.vercel.app/",
+      "https://creditoya.vercel.app/dashboard",
+      "https://intranet-creditoya.vercel.app/",
+      "https://intranet-creditoya.vercel.app/dashboard",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
